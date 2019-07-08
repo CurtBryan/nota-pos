2f9eb604750336e3436e051271ff4f35614a407c
Curt Bryan <kurdtbryan@gmail.com>
2019-07-08 16:37:09 -0700

    doen with auth yay

---
+++ server/controller/authController.js
@@ -0,0 +1,58 @@
+const bcrypt = require("bcrypt");
+const Restaurant = require("../collections/restaurant");
+
+module.exports = {
+  login: (req, res, next) => {
+    const { typedEmail, typedPassword } = req.body;
+    // console.log(typedEmail, typedPassword);
+    // const query = { email: `${typedEmail}`, password: `${typedPassword}` };
+    Restaurant.find({ email: typedEmail })
+      .then(restaurant => {
+        bcrypt
+          .compare(typedPassword, restaurant[0].password)
+          .then(matchedPassword => {
+            if (matchedPassword) {
+              req.session.user = restaurant[0].name;
+              res.status(200).send(req.session.user);
+            } else {
+              res.status(401).send("incorrect info mang");
+            }
+          });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(401).send("incorrect info mang");
+      });
+  },
+  register: (req, res, next) => {
+    const { typedName, typedEmail, typedPassword } = req.body;
+    const saltRounds = 12;
+    bcrypt.genSalt(saltRounds).then(salt => {
+      bcrypt.hash(typedPassword, salt).then(hashedPassword => {
+        const restaurant = new Restaurant({
+          name: typedName,
+          email: typedEmail,
+          password: hashedPassword
+        });
+        restaurant.save(err => {
+          if (err) {
+            res
+              .status(400)
+              .send("there was an error on the server", console.log(err));
+          }
+          Restaurant.find({ email: typedEmail }).then(restaurant => {
+            req.session.user = restaurant[0].name;
+            res.status(200).send(req.session.user);
+          });
+        });
+      });
+    });
+  },
+  userInfo: (req, res, next) => {
+    res.status(200).send(req.session.user);
+  },
+  logout: (req, res, next) => {
+    req.session.destroy();
+    res.status(200).send("logged out");
+  }
+};
---
+++ server/index.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const app = express();
+
+app.use(express.static(__dirname + "/build"));
+
+app.use(express.json());
+
+const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env;
+
+const {
+  login,
+  register,
+  userInfo,
+  logout
+} = require("./controller/authController");
+
+app.use(
+  session({
+    saveUninitialized: false,
+    secret: SESSION_SECRET,
+    resave: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 14 // twoweeks
+    }
+  })
+);
+
+mongoose
+  .connect(CONNECTION_STRING, { useNewUrlParser: true, useCreateIndex: true })
+  .then(() => console.log("mongo connected"))
+  .catch(() => console.log("mongo failed"));
+
+app.get("/api/user", userInfo);
+app.post("/api/register", register);
+app.post("/api/login", login);
+app.get("/api/logout", logout);
+
+const port = SERVER_PORT || 4000;
+app.listen(port, () => console.log(`server up and running on ${port}`));
