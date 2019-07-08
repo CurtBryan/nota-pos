7f5e9ceda322745a62527a63c9c93081311872ab
Ryland McGee <mcgee.reilly@gmail.com>
2019-07-08 15:38:16 -0800

    maybe this will make the menu work

---
+++ server/controller/menuController.js
@@ -0,0 +1,95 @@
+const menu = require("../collections/menu");
+module.exports = {
+  getRestaurantMenu: (req, res, next) => {
+    const { restaurant } = req.params;
+    menu
+      .find({ restaurant: restaurant }, { division: 1, items: 1 })
+      .then(response => {
+        res.status(200).send(response);
+      })
+      .catch(err => console.log(err));
+  },
+  postDivisions: (req, res, next) => {
+    const { restaurant, division, item } = req.body;
+    menu
+      .save({ restaurant: restaurant, division: division, items: item })
+      .then(() => {
+        menu
+          .find({ restaurant: restaurant }, { division: 1, items: 1 })
+          .then(response => {
+            res.status(200).send(response);
+          });
+      });
+  },
+  updateDivisions: (req, res, next) => {
+    const { restaurant, division, newdiv } = req.body;
+    menu
+      .findOne({ restaurant: restaurant, division: division })
+      .then(record => {
+        record.division = newdiv;
+        record.save().then(() => {
+          menu
+            .find({ restaurant: restaurant }, { division: 1, items: 1 })
+            .then(response => {
+              res.status(200).send(response);
+            });
+        });
+      });
+  },
+  deleteDivision: (req, res, next) => {
+    const { restaurant, division } = req.body;
+    menu.remove({ restaurant: restaurant, division: division }).then(() => {
+      menu
+        .find({ restaurant: restaurant }, { division: 1, items: 1 })
+        .then(response => {
+          res.status(200).send(response);
+        });
+    });
+  },
+  postItem: (req, res, next) => {
+    const { restaurant, division, item, price } = req.body;
+    menu
+      .findOne({ restaurant: restaurant, division: division })
+      .then(record => {
+        record.items.push({ item: item, price: price });
+        record.save().then(() => {
+          menu
+            .find({ restaurant: restaurant }, { division: 1, items: 1 })
+            .then(response => {
+              res.status(200).send(response);
+            });
+        });
+      })
+      .catch(err => console.log(err));
+  },
+  updateItem: (req, res, next) => {
+    const { restaurant, division, item, price } = req.body;
+    menu
+      .findOne({ restaurant: restaurant, division: division })
+      .then(record => {
+        record.items[record.items.indexOf(item)].price = price;
+        record.save().then(() => {
+          menu
+            .find({ restaurant: restaurant }, { division: 1, items: 1 })
+            .then(response => {
+              res.status(200).send(response);
+            });
+        });
+      });
+  },
+  deleteItem: (req, res, next) => {
+    const { restaurant, division, item } = req.body;
+    menu
+      .findOne({ restaurant: restaurant, division: division })
+      .then(record => {
+        record.items.splice(record.items.indexOf(item), 1);
+        record.save().then(() => {
+          menu
+            .find({ restaurant: restaurant }, { division: 1, items: 1 })
+            .then(response => {
+              res.status(200).send(response);
+            });
+        });
+      });
+  }
+};
