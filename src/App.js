4665739f80f034de0270a525852396204481ba38
Curt Bryan <kurdtbryan@gmail.com>
2019-07-09 15:30:10 -0700

    cookview frame

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import Register from './Components/Register/Register';
-import './App.css';
+import React from "react";
+import Register from "./Components/Register/Register";
+import CookView from "./Components/CookView/CookView";
+import "./App.css";
 
+/*
+Red = #D84848
+Blue = #62B8EF
+Green = #A0F290
+Yellow = #FCC751 opacity 70%
+lightgray= #EDEDED
+darkgray=#585555
+#547374 Background Color
+
+*/
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <Register />
+        <CookView />
       </header>
     </div>
   );
