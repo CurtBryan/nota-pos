160a6aec0b6219ed630c2743c2a8f4088a8e784d
Destiny Jones <destinyjones@Destinys-MacBook.local>
2019-07-10 09:13:14 -0700

    save changes

--- src/Components/ServerView/ServerView.js
+++ src/Components/ServerView/ServerView.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { setUser } from '../../ducks/userReducer';
 import { connect } from 'react-redux';
+import { FaAngleDoubleLeft, FaAngleDoubleRight } from "react-icons/fa";
 import './ServerView.css';
 
 
@@ -12,47 +13,76 @@ class ServerView extends Component {
         }
     }
 
+
+
     render(){
         return(
             <div className="server-page" >
                 <div className="table-container">
-                    <div className="table"></div>
-                    <div className="table"></div>
-                    <div className="table"></div>
-                    <div className="table"></div>
-                    <div className="table"></div>
+                    <button className="tableNavButtons">
+                     <FaAngleDoubleLeft />
+                    </button>
+                    <button className="table"></button>
+                    <button className="table"></button>
+                    <button className="table"></button>
+                    <button className="table"></button>
+                    <button className="table"></button>
+                    <button className="tableNavButtons">
+                     <FaAngleDoubleRight />
+                    </button>
                 </div>
-                <div className="items-container">
-                    <header className="menu-items">
-                        <div className="box item1"></div>
-                        <div className="box item1"></div>
-                        <div className="box item1"></div>
-                        <div className="box item1"></div>
-                        <div className="box item1"></div>
-                    </header>
-                    <header className="menu-items">
-                        <div className="box item2"></div>
-                        <div className="box item2"></div>
-                        <div className="box item2"></div>
-                        <div className="box item2"></div>
-                        <div className="box item2"></div>
-                    </header>
-                    <header className="menu-items">
-                        <div className="box item3"></div>
-                        <div className="box item3"></div>
-                        <div className="box item3"></div>
-                        <div className="box item3"></div>
-                        <div className="box item3"></div>
-                    </header>
-                    <header className="menu-items">
-                        <div className="box item4"></div>
-                        <div className="box item4"></div>
-                        <div className="box item4"></div>
-                        <div className="box item4"></div>
-                        <div className="box item4"></div>
-                    </header>
+                <div className="menu-selections">
+                    <div className="circle-container">
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                        <div className="circle"></div>
+                    </div>
                 </div>
+                <div className="info-section">
+                    <div className="items-container">
+                        <section className="menu-items">
+                            <div className="box item1"></div>
+                            <div className="box item1"></div>
+                            <div className="box item1"></div>
+                            <div className="box item1"></div>
+                            <div className="box item1"></div>
+                        </section>
+                        <section className="menu-items">
+                            <div className="box item2"></div>
+                            <div className="box item2"></div>
+                            <div className="box item2"></div>
+                            <div className="box item2"></div>
+                            <div className="box item2"></div>
+                        </section>
+                        <section className="menu-items">
+                            <div className="box item3"></div>
+                            <div className="box item3"></div>
+                            <div className="box item3"></div>
+                            <div className="box item3"></div>
+                            <div className="box item3"></div>
+                        </section>
+                        <section className="menu-items">
+                            <div className="box item4"></div>
+                            <div className="box item4"></div>
+                            <div className="box item4"></div>
+                            <div className="box item4"></div>
+                            <div className="box item4"></div>
+                        </section>
+                    </div>
+                    <div className="tickets">
+                        <p className="title">POS-SYSTEM</p>
+                        <div className="bar"></div>
+                        <div className="ticket"></div>
+                        <div className="price-tag"></div>
+                    </div>
+                    
+                </div>
+                <footer>
+                    <button className="logout"> LOGOUT </button>
+                </footer>
             </div>
+
         )
     }
 }
